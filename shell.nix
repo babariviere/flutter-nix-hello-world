3ac9d051584f40b15e33cb3056e3f9b8b20d9cd4
Bastien Rivière <babathriviere@gmail.com>
2020-01-10 18:49:11 +0100

    Add ANDROID_HOME to build apk
    Closes #1

--- shell.nix
+++ shell.nix
@@ -5,4 +5,6 @@ let
     "https://github.com/babariviere/nixpkgs/archive/flutter-init.tar.gz") {}).flutter;
 in pkgs.mkShell {
   buildInputs = [ flutter ];
+
+  ANDROID_HOME = "${pkgs.androidsdk_9_0}/libexec/android-sdk";
 }
