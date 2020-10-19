6f1cbaaf061bc331645d44a292c0e7e5f68418f7
nanashi0x74 <rian.lindenberger@mpq.se>
2020-10-19 13:43:29 +0200

    Update niv, sources, android and flutter

--- nix/android.nix
+++ nix/android.nix
@@ -2,21 +2,17 @@
 
 androidenv.composeAndroidPackages {
   toolsVersion = "25.2.5";
-  platformToolsVersion = "28.0.1";
+  platformToolsVersion = "29.0.6";
   buildToolsVersions = [ "28.0.3" ];
   includeEmulator = true;
-  emulatorVersion = "28.0.14";
+  emulatorVersion = "30.0.3";
   platformVersions = [ "28" ];
   includeSources = false;
   includeDocs = false;
   includeSystemImages = true;
   systemImageTypes = [ "default" ];
   abiVersions = [ "x86_64" ];
-  # lldbVersions = [ "2.0.2558144" ];
-  # cmakeVersions = [ "3.6.4111459" ];
   includeNDK = false;
-  # ndkVersion = "16.1.4479499";
   useGoogleAPIs = false;
   useGoogleTVAddOns = false;
-  # includeExtras = [ "extras;google;gcm" ];
 }
